b286e9c919001ca66102232973497166519848c4
R3SONANC3 <jeerapat5870@gmail.com>
2024-05-24 20:25:34 +0700

    update frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist', // Specify the correct output directory
-  },
-});
+  plugins: [react()],
+})
